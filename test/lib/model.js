b6d3502c7c65b1aef63627460e06d1576dc679fb
Roland Sharp <rolandnsharp@gmail.com>
2015-01-11 02:21:54 +1100

    export the model

---
+++ test/lib/model.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.connect(process.env.MONGOOSE_TEST_URI || 'mongodb://localhost/test');
+
+var mongooseSoftDelete = require('../../');
+
+var schema = new Schema({
+    name: String
+}, {
+    collection: 'testCollection'
+});
+
+schema.plugin(mongooseSoftDelete);
+
+module.exports = mongoose.model('Test', schema);

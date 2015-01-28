7fae830c5a1d8c3b1d27047ea0ed906758a3a905
Roland Sharp <rolandnsharp@gmail.com>
2015-01-29 09:34:05 +1100

    Comment out mongoose-fixtures loading.

--- test/index.js
+++ test/index.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var fixtures = require('mongoose-fixtures');
 
 // Before each test, load the fixture data to ensure consistent tests.
-var data = require('./lib/fixtures');
-beforeEach(function beforeEachTest(done) {
-    fixtures.load(data, done);
-});
+// var data = require('./lib/fixtures');
+// beforeEach(function beforeEachTest(done) {
+//     fixtures.load(data, done);
+// });
 
 after(function(done) {
     mongoose.connection.db.dropCollection("testCollection", done);

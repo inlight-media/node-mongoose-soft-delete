104539527729e8090dad10940634bdd3bd3f0600
Roland Sharp <rolandnsharp@gmail.com>
2015-01-29 09:32:39 +1100

    Add temporary manual fixtures loading for count-test.

--- test/count-test.js
+++ test/count-test.js
@@ -5,12 +5,36 @@ var fixtures = require('./lib/fixtures');
 var mongoose = require('mongoose');
 
 describe("count(); ", function() {
-	var test = fixtures.test.default;
+	var test;
+    var test1;
+    var test2;
+    var test3;
+    var test4;
+
+    before(function(done) {
+        test = new Test(fixtures.test.default);
+        test1 = new Test(fixtures.test.default1);
+        test2 = new Test(fixtures.test.default2);
+        test3 = new Test(fixtures.test.default3);
+        test4 = new Test(fixtures.test.default4);
+
+        test.save(function() {
+            test1.save(function() {
+                test2.save(function() {
+                    test3.save(function() {
+                        test4.save(function() {
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
 
 	it("Should not count soft deleted documents.", function(done) {
 		Test.count(function(err, count) {
-			count.should.be.exactly(4).and.be.a.Number;
 			should.not.exist(err);
+			count.should.be.exactly(4).and.be.a.Number;
 
 			done();
 		});

61afd680e1328a8a306fd4bdae66603b21102083
Roland Sharp <rolandnsharp@gmail.com>
2015-05-01 09:58:46 +1000

    Add better fixtures names.

--- test/lib/fixtures.js
+++ test/lib/fixtures.js
@@ -5,25 +5,27 @@ var Test = require('./model');
 var test = {
     default: {
         _id: new ObjectId(),
-        name: 'test'
+        name: 'default'
     },
-    default1: {
+    archived: {
+        _id: new ObjectId(),
+        name: 'archived',
+        archived: true,
+        archivedAt: new Date()
+    },
+    removed: {
+        _id: new ObjectId(),
+        name: 'removed',
+        deleted: true,
+        deletedAt: new Date()
+    },
+    test1: {
         _id: new ObjectId(),
         name: 'test1'
     },
-    default2: {
+    test2: {
         _id: new ObjectId(),
         name: 'test2'
-    },
-    default3: {
-        _id: new ObjectId(),
-        name: 'test3'
-    },
-    default4: {
-        _id: new ObjectId(),
-        name: 'test4',
-        deleted: true,
-        deletedAt: new Date()
     }
 };
 

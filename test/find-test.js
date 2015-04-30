da4ce9c3649b8120bf51ddf0fe66801ab8c39758
Roland Sharp <rolandnsharp@gmail.com>
2015-04-30 15:58:31 +1000

    Add extra condition testing for the .find query
    if {archived: null}, {archived: true}, {deleted: true}

--- test/find-test.js
+++ test/find-test.js
@@ -1,27 +1,65 @@
-var _ = require('lodash');
-var should = require('should');
 var Test = require('./lib/model');
-var fixtures = require('./lib/fixtures');
-var mongoose = require('mongoose');
+var should = require('should');
 
 describe("find(); ", function() {
-	var test = new Test(fixtures.test.default);
 
-	it("Should not return document after it is soft deleted.", function(done) {
-		Test.remove({
-			_id: test._id
-		}, function(err) {
-			Test.find(function(err, tests) {
+	it("should not return `removed` or `archived` documents by default", function(done) {
 
-				should.not.exist(err);
-				tests.should.be.instanceof(Array).and.have.lengthOf(3);
-				tests.forEach(function(test) {
-					should.not.exist(test.deleted);
-
-				});
-				done();
+		Test.find(function(err, tests) {
+			should.not.exist(err);
+			tests.should.be.instanceof(Array).and.have.lengthOf(3);
+			tests.forEach(function(test) {
+				should.not.exist(test.deleted);
+				should.not.exist(test.archived);
 			});
+			done();
 		});
 	});
 
+	it("shoul only return archived documents when using flag `{archived: true}`", function(done) {
+
+		Test.find({
+			archived: true
+		}, function(err, tests) {
+
+			should.not.exist(err);
+			tests.should.be.instanceof(Array).and.have.lengthOf(1);
+			tests.forEach(function(test) {
+				test.archived.should.be.true;
+				should.not.exist(test.deleted);
+			});
+			done();
+		});
+	});
+
+	it("shoul return archived documents AND and documents that are neither `archived` nor `removed` when using the flag `{archived: null}`", function(done) {
+
+		Test.find({
+			archived: null
+		}, function(err, docs) {
+			should.not.exist(err);
+			docs.should.be.instanceof(Array).and.have.lengthOf(4);
+			// should contain one archived document
+			docs.forEach(function(doc) {
+				should.not.exist(doc.deleted);
+			});
+			done();
+		});
+	});
+
+	it("shoul only return deleted documents when using flag `{deleted: ture}`", function(done) {
+
+		Test.find({
+			deleted: true
+		}, function(err, tests) {
+
+			should.not.exist(err);
+			tests.should.be.instanceof(Array).and.have.lengthOf(1);
+			tests.forEach(function(test) {
+				test.deleted.should.be.true;
+				should.not.exist(test.archived);
+			});
+			done();
+		});
+	});
 });

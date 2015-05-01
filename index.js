6f6fcff6996d693f74ea869c3290054bf1cbb9f4
Roland Sharp <rolandnsharp@gmail.com>
2015-05-01 11:14:09 +1000

    Add archived and removed query flag functionality.

--- index.js
+++ index.js
@@ -1,30 +1,94 @@
 module.exports = function(schema) {
 
 	schema.add({
-		deleted: Boolean,
-		deletedAt: Date,
+		removed: Boolean,
+		removedAt: Date,
 		archived: Boolean,
 		archivedAt: Date
 	});
 
-	var hooks = ['find', 'findOne', 'findOneAndUpdate', 'update', 'count'];
+	var queries = ['find', 'findOne', 'findOneAndUpdate', 'update', 'count'];
 
-	hooks.forEach(function(hook) {
-		schema.pre(hook, function(next) {
+	queries.forEach(function(query) {
+		schema.pre(query, function(next) {
 
-			this.where({
-				deleted: {
-					'$ne': true
-				},
-				archived: {
+			var conditions = {
+				removed: {
 					'$ne': true
 				}
-			});
+			};
+
+			if (this._conditions.archived === null) {
+				// remove invalid mongoose condition {archived: null}
+				delete this._conditions.archived;
+				this.where(conditions);
+
+				return next();
+			}
+
+			conditions.archived = {
+				'$ne': true
+			}
+
+			this.where(conditions);
 			next();
 		});
 	});
 
+	schema.statics.destroy = function() {
+	// save instance of remove
+		// TODO fix this
+		// return this.remove.apply(this, arguments);
+	};
+
 	schema.statics.remove = function(first, second) {
+		// TODO if archived. removed from archived
+		var callback;
+		var update = {
+			removed: true,
+			removedAt: new Date()
+		};
+		var conditions;
+		var options = {
+			multi: false
+		};
+
+		if (typeof first === 'function') {
+			callback = first;
+			conditions = {};
+			options.multi = true;
+		} else {
+			callback = second;
+			conditions = first;
+		}
+
+		if (typeof callback !== 'function') {
+			throw ('Wrong arguments!');
+		}
+
+		this.update(conditions, update, options, function(err, numAffected) {
+			if (err) {
+				return callback(err);
+			}
+			callback(null, numAffected);
+		});
+	};
+
+	schema.methods.remove = function(callback) {
+		// TODO test this conditional.
+		// when document is removed, it is no longer archived
+		if (this.archived) {
+			this.archived = undefined;
+			this.archivedAt = undefined;
+		}
+
+		this.removed = true;
+		this.removedAt = new Date();
+		this.save(callback);
+	};
+
+
+	schema.statics.restore = function(first, second) {
 		var callback;
 		var conditions;
 
@@ -41,11 +105,15 @@ module.exports = function(schema) {
 		}
 
 		var update = {
-			deleted: true,
-			deletedAt: new Date()
+			$unset: {
+				removed: 1
+			},
+			$unset: {
+				removedAt: 1
+			}
 		};
-
-		this.update(conditions, update, function(err, numberAffected) {
+		// TODO use mongoose .update
+		this.collection.update(conditions, update, function(err, numberAffected) {
 			if (err) {
 				return callback(err);
 			}
@@ -57,20 +125,14 @@ module.exports = function(schema) {
 		});
 	};
 
-	schema.methods.remove = function(first, second) {
-		var callback = typeof first === 'function' ? first : second;
-
-		if (typeof callback !== 'function') {
-			throw ('Wrong arguments!');
-		}
-
-		this.deleted = true;
-		this.deletedAt = new Date();
-
+	schema.methods.restore = function(callback) {
+		this.removed = undefined;
+		this.removedAt = undefined;
 		this.save(callback);
 	};
 
 	schema.statics.archive = function(first, second) {
+
 		var callback;
 		var conditions;
 
@@ -91,14 +153,13 @@ module.exports = function(schema) {
 			archivedAt: new Date()
 		};
 
-		this.update(conditions, update, function(err, numberAffected) {
+		this.update(conditions, update, function(err, raw) {
+
 			if (err) {
 				return callback(err);
 			}
-			if (numberAffected === 0) {
-				return callback('Wrong arguments!');
-			}
-			callback(null);
+			// `raw` is raw mongo output
+			callback(null, raw);
 
 		});
 	};
@@ -113,6 +174,48 @@ module.exports = function(schema) {
 		this.archived = true;
 		this.archivedAt = new Date();
 
+		this.save(callback);
+	};
+
+	schema.statics.unarchive = function(first, second) {
+		var callback;
+		var conditions;
+
+		if (typeof first === 'function') {
+			callback = first;
+			conditions = {};
+		} else {
+			callback = second;
+			conditions = first;
+		}
+
+		if (typeof callback !== 'function') {
+			throw ('Wrong arguments!');
+		}
+
+		var update = {
+			$unset: {
+				archived: 1,
+				archivedAt: 1
+			}
+		};
+		// TODO update with mongoose
+		this.collection.update(conditions, update, function(err, raw) {
+			if (err) {
+				return callback(err);
+			}
+			if (raw === 0) {
+				return callback('Wrong arguments!');
+			}
+			// `raw` is raw mongo output
+			callback(null, raw);
+
+		});
+	};
+
+	schema.methods.unarchive = function(callback) {
+		this.archived = undefined;
+		this.archivedAt = undefined;
 		this.save(callback);
 	};
 };

461a9adc21e4b2de12bc4f8d31f39c5895e774c8
Roland Sharp <rolandnsharp@gmail.com>
2015-05-01 13:33:55 +1000

    Update testing for .unarchive

--- test/unarchive-test.js
+++ test/unarchive-test.js
@@ -4,22 +4,22 @@ var Test = require('./lib/model');
 var fixtures = require('./lib/fixtures');
 var mongoose = require('mongoose');
 
-describe(".unarchive(); Method:", function() {
+describe("unarchive(); Method:", function() {
 
-    it("Should unarchive all archived documents.");
+	it("Should unarchive all archived documents.");
 
 	it("Should unarchive a archived document.", function(done) {
 
-		Test.archive({
-			name: 'default'
+		Test.unarchive({
+			name: 'archived'
 		}, function(err, docs) {
 			should.not.exist(err);
-
+			// TODO test for returned unarchived document if we decide to implememt this feature
 			Test.findOne({
-				name: 'default'
+				name: 'archived',
 			}, function(err, doc) {
 				should.not.exist(err);
-				should.not.exist(doc);
+				doc.name.should.be.exactly('archived');
 
 				done();
 			});
@@ -27,23 +27,28 @@ describe(".unarchive(); Method:", function() {
 	});
 });
 
-describe(".unarchive(); Static:", function() {
+describe("unarchive(); Static:", function() {
 
-	it("Should unarchive a archived document.", function(done) {
-		var test4 = new Test(fixtures.test.default4);
-		test4.archived.should.be.true;
-		Test.unarchive({
-			name: 'test4'
+	it("Should unarchive an archived document.", function(done) {
+
+		Test.findOne({
+			archived: true,
+			name: 'archived'
 		}, function(err, doc) {
-			Test.find({
-				name: 'test4'
-			}, function(err, test4) {
-				should.not.exist(err);
-				should.not.exist(test4[0].archived);
+			doc.archived.should.be.true;
 
-				done();
+			doc.unarchive(function(err, doc) {
+				should.not.exist(err);
+				Test.findOne({
+					name: 'archived'
+				}, function(err, doc) {
+                    should.not.exist(err);
+                    doc.should.have.properties({name: 'archived'});
+                    should.not.exist(doc.archived);
+
+					done();
+				});
 			});
 		});
-
 	});
 });
